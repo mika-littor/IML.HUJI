05ec76202db55c058d35b031a4eae1459d1aeca1
mika.li <mika.littor@mail.huji.ac.il>
2023-04-26 18:09:38 +0300

    commit hw2 - house pricing

--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -1,4 +1,6 @@
 # CSE: mika.li 322851593
+import numpy
+
 from IMLearn.utils import split_train_test
 from IMLearn.learners.regressors import LinearRegression
 
@@ -13,6 +15,7 @@ pio.templates.default = "simple_white"
 
 # global variable for the zipcodes in the train set
 LST_ZIPCODES_COL = []
+
 
 def preprocess_on_invalid_pricing(X, y):
     """
@@ -53,14 +56,15 @@ def preprocess_only_on_train_data(df):
     """
     # remove rows with nan values in the dataframe
     df = df.dropna(axis=0)
+    # dropping duplicates
+    df = df.drop_duplicates()
     # filter the database values according to the column
-    df = df.query("0<price and 0<sqft_living and 0<sqft_lot and 0<sqft_above "
+    df = df.query("0<price and 0<sqft_living and 0<sqft_lot and 0<sqft_above and 0<sqft_living15 "
                   "and 0<=floors and 0<=sqft_basement "
                   "and 1900<=yr_built<=2023 "
                   "and 0<=yr_renovated<=2023 "
                   "and 0<=bedrooms<=30 "
                   "and 0<=bathrooms<=10 "
-                  "and 0<sqft_lot "
                   "and waterfront in (0,  1) "
                   "and 0<=view<=4 "
                   "and 1<=condition<=5 "
@@ -74,7 +78,7 @@ def preprocess_only_on_train_data(df):
 
 def preprocess_only_on_test_data(X):
     """
-    preprocessing of the test data by using the LST_ZIPCODES created by the train part to encode the zipcodes
+    using the LST_ZIPCODES created by the train set to encode the zipcodes
 
     Parameters
     ----------
@@ -94,6 +98,7 @@ def preprocess_only_on_test_data(X):
     X = X.drop(columns="zipcode", axis=1)
     return X
 
+
 def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     """
     preprocess data
@@ -111,7 +116,7 @@ def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     DataFrame or a Tuple[DataFrame, Series]
     """
     # remove irrelevant columns or columns that have negative correlation with the price
-    X = X.drop(["id", "date", "sqft_lot15", "sqft_living15", "lat", "long"], axis=1)
+    X = X.drop(["id", "date", "lat", "long", "sqft_lot15"], axis=1)
     # create a column indicates if the building was built or renovated in the past 30 years
     X["renewed_this_century"] = ((X["yr_renovated"] >= 2000) | (X["yr_built"] >= 2000))
     X["renewed_this_century"] = X["renewed_this_century"].astype(int)
@@ -127,6 +132,7 @@ def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
         return X, y
     else:
         return preprocess_only_on_test_data(X)
+
 
 def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") -> NoReturn:
     """
@@ -145,15 +151,26 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") ->
     output_path: str (default ".")
         Path to folder in which plots are saved
     """
-    raise NotImplementedError()
+    # drop the zipcode hot coding as every column matches a few samples
+    X = X.drop(X.filter(regex='^zc_'), axis=1)
+
+    for feature in X:
+        # calculate the correlation
+        cov_matrix_feature_response = np.cov(X[feature], y)
+        cov_val_feature_response = cov_matrix_feature_response[1, 0]
+        denominator = np.std(X[feature]) * np.std(y)
+        corr = cov_val_feature_response / denominator
+
+        px.scatter(pd.DataFrame({'x_axis': X[feature], 'y_axis': y}), x="x_axis", y="y_axis", trendline="ols",
+                   title=f"{feature} vs Response of Pearson Correlation={corr}",
+                   labels={"x_axis": f"feature: {feature}", "y_axis": "Response"},
+                   color_discrete_sequence=["blue"],
+                   trendline_color_override="green").write_image(output_path + f"/pearson_corr_{f}.png")
 
 
 if __name__ == '__main__':
     np.random.seed(0)
     df = pd.read_csv("../datasets/house_prices.csv")
-
-    # todo: remove command
-    df = pd.read_csv(r"C:\Users\mikal\Documents\CS4\IML\IML.HUJI\TESTS\house_prices_test.csv").head(20)
 
     # Question 1 - split data into train and test sets
     train_proportion = 0.75
@@ -167,12 +184,11 @@ if __name__ == '__main__':
     test_x, test_y = preprocess_on_invalid_pricing(test_x, test_y)
     # preprocess on the train set
     train_x, train_y = preprocess_data(train_x, train_y)
-    print("train x:\n", train_x)
     # preprocess on the test set
     test_x = preprocess_data(test_x)
-    print("test x:\n", test_x)
 
     # Question 3 - Feature evaluation with respect to response
+    # feature_evaluation(train_x, train_y)
 
     # Question 4 - Fit model over increasing percentages of the overall training data
     # For every percentage p in 10%, 11%, ..., 100%, repeat the following 10 times:
@@ -181,3 +197,34 @@ if __name__ == '__main__':
     #   3) Test fitted model over test set
     #   4) Store average and variance of loss over test set
     # Then plot average loss as function of training size with error ribbon of size (mean-2*std, mean+2*std)
+
+    lst_p_val = list(range(10, 101))
+    lst_mean_val = []
+    lst_sd_val = []
+    for p in lst_p_val:
+        # insert into a list 10 times the loss calculation over the current p fraction of samples of the data
+        lst_samples_loss = []
+        for i in range(10):
+            X_sample = train_x.sample(frac=p / 100.0)
+            y_sample = train_y.loc[X_sample.index]
+            regression_val = LinearRegression(include_intercept=True).fit(X_sample, y_sample)
+            loss_val = regression_val.loss(test_x, test_y)
+            lst_samples_loss.append(loss_val)
+        # calculate the mean and sd of the loss
+        lst_mean_val.append(np.mean(lst_samples_loss))
+        lst_sd_val.append(np.std(lst_samples_loss))
+
+    means = numpy.array(lst_mean_val)
+    sds = numpy.array(lst_sd_val)
+
+    upper_border = means + 2 * sds
+    lower_border = means - 2 * sds
+    go.Figure([go.Scatter(x=lst_p_val, y=upper_border, fill=None, mode="lines", line=dict(color="lightblue")),
+                     go.Scatter(x=lst_p_val, y=lower_border, fill='tonexty', mode="lines", line=dict(color="lightblue")),
+                     go.Scatter(x=lst_p_val, y=lst_mean_val, mode="markers+lines", marker=dict(color="blue"))],
+                    layout=go.Layout(title="MSE of test set over different sample sizes of the train set",
+                                     xaxis=dict(title="Sample Percentage (Training Set)"),
+                                     yaxis=dict(title="MSE (Test Set)"),
+                                     showlegend=False))
+    fig.show()
+    # fig.write_image("mse.over.training.percentage.png")
